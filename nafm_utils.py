787d0d64e2c7f331f706c1bcffc4297f2a3e3ee7
Rutanshu Desai <rutanshudesai@gmail.com>
2020-06-10 18:14:22 -0400

    redefined the comma_create_function

--- nafm_utils.py
+++ nafm_utils.py
@@ -16,12 +16,39 @@ def data_from_RS(query, want_csv_file = False, csv_file_name=None):
     return data 
 
 
-def create_quote_comma_text():
+def create_quote_comma_text(csv_output=False, csv_name='quotes_comma'):
     '''
     This function is created so that simple space separated texts can be converted to quotes-comma text so that they can be used   as filters in SQL or any other usage.
-
-    It requires no parameters, just input your text data which may be Email Addresses, offerIDs or any else. 
+    -------
+    Parameters
+    -------
+    csv_output: Default is False and will print a list in Jupyter Notebook. 
+                If user inputs True, then a CSV will be created as output.
+                
+    csv_name: This parameter will be used only if csv_output is True.
+              Default CSV name is 'quotes_comma'. User can input other name as desired
     '''
+    ## inputing the raw data
     raw_data=input('Input your data which is space separated: ')
-    texts = raw_data.split()
-    return texts
+    raw_texts = raw_data.split()
+    
+    ## cleaning the text if it has a quote in it
+    cleaned_texts=[]
+    for text in raw_texts:
+        if "'" in text:
+            text=text.replace("'", "''")
+            cleaned_texts.append(text)
+        else:
+            cleaned_texts.append(text)
+
+    ## adding quotes and comma to a cleaned list        
+    final_text_list=[]
+    for text in cleaned_texts:
+        text="'"+text+"'"+","
+        final_text_list.append(text)
+    
+    ## return the final list as a csv for easy use if the user wants it
+    if csv_output is True:
+        df=pd.DataFrame(final_text_list)
+        df.to_csv(path_or_buf= csv_name+'.csv')
+    return final_text_list
