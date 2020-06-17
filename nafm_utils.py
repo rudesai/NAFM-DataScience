3ec5e8f2ecc9ac37d56c96e6d91ce3b039e55343
Rutanshu Desai <rutanshudesai@gmail.com>
2020-06-17 17:08:36 -0400

    minor tweak based upon whether you want to download the file or not

--- nafm_utils.py
+++ nafm_utils.py
@@ -15,7 +15,6 @@ def data_from_RS(query, want_csv_file = False, csv_file_name=None):
         data.to_csv(csv_file_name)
     return data 
 
-
 def create_quote_comma_text(csv_output=False, csv_name='quotes_comma'):
     '''
     This function is created so that simple space separated texts can be converted to quotes-comma text so that they can be used   as filters in SQL or any other usage.
@@ -40,19 +39,22 @@ def create_quote_comma_text(csv_output=False, csv_name='quotes_comma'):
             cleaned_texts.append(text)
         else:
             cleaned_texts.append(text)
-
-    final_text_list=[]
-    for text in cleaned_texts:
-        ## if the item is the last item in the list then don't add the final comma
-        if text == cleaned_texts[-1]:
-            text="'"+text+"'"
-            final_text_list.append(text)
-        else:
-            text="'"+text+"'"+","
-            final_text_list.append(text)
     
-    ## return the final list as a csv for easy use if the user wants it
-    if csv_output is True:
-        df=pd.DataFrame(final_text_list)
-        df.to_csv(path_or_buf= csv_name+'.csv')
-    return final_text_list
+    if csv_output==False:
+        print (cleaned_texts)
+    else:
+        final_text_list=[]
+        for text in cleaned_texts:
+            ## if the item is the last item in the list then don't add the final comma
+            if text == cleaned_texts[-1]:
+                text="'"+text+"'"
+                final_text_list.append(text)
+            else:
+                text="'"+text+"'"+","
+                final_text_list.append(text)
+
+        ## return the final list as a csv for easy use if the user wants it
+        if csv_output is True:
+            df=pd.DataFrame(final_text_list)
+            df.to_csv(path_or_buf= csv_name+'.csv')
+        return print('See the downloaded file titled "{}.csv"'.format(csv_name))
