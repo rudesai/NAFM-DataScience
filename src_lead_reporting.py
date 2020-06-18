f9ded261ffcfd260b90c6a41f7f14c03665bd1fc
Rutanshu Desai <rutanshudesai@gmail.com>
2020-06-18 18:56:57 -0400

    created initial v1 class and methods for creating table and calcs

--- src_lead_reporting.py
+++ src_lead_reporting.py
@@ -1,76 +1,133 @@
 import datascience as ds
 import pandas as pd
+import datetime as dt
 
 
 ## GETTING THE LEADS SPECIFC TO A PERSONA AND A YEAR
 
         ## CREATE A FUNCTION HERE WHICH TAKES IN PARAMETERS FOR HOW YOU WANT TO CREATE THE INITIAL LEADS DATASET
-try:
-    query='''
-            create table na_field_marketing.temp_leads_ite_2018 as (
-            select   distinct lead_id 
-            from na_field_marketing.src_leads_sfdc l
-            join 
-            (
-            select distinct "lead contact id"
-            from na_field_marketing.prod_nafm_mt pnm
-            where "eloqua persona" = 'IT Executive' and "email (person)" != '%ibm%'
-            ) p
-            on left(l.lead_id,15) = "lead contact id"
-            where lead_date_created >= '2018-01-01' and lead_date_created <= '2018-12-31'
-            );
-            '''
-    ds.query_RS(query,option= 'execute')
-except:
-    query='drop table na_field_marketing.temp_leads_ite_2018;'+query
-    ds.query_RS(query,option= 'execute')    
 
-print('--------- Lead Report for IT Executive for the year 2018 ---------')    
+class lead_reporting_persona:
     
+    ## CREATING TABLE
+    def create_table(self):
+        query='''
+               select distinct "eloqua persona"
+               from na_field_marketing.prod_nafm_mt
+              '''
+        df=ds.query_RS(query)
 
-       ## CREATE A FUNCTION FOR TOTAL LEADS
-## CALCULTION OF TOTAL LEADS
-query='''
-select count(distinct l.lead_id) as leads
-from na_field_marketing.src_lmh_sfdc l
-join 
-na_field_marketing.temp_leads_ite_2018 t
-on l.lead_id = t.lead_id
-;
-    '''
-df=ds.query_RS(query)
-print('Total number of leads: {}'.format(df['leads'][0]))
+        ## CREATING PERSONA LIST OUT OF THE DATAFRAME
+        series=df['eloqua persona']
+        personas=[]
+        for item in series:
+            item=item.replace(' ','_')
+            personas.append(item)
+        
 
-
-       ## CREATE A FUNCTION FOR MALS SALS SQLS              
-## CALC FOR MALS SALS SQLS
-
-lead_stages=['Marketing Accepted','Sales Accepted','Sales Qualified']
-for lead_stage in lead_stages:
-    query='''
-        select  count(distinct l.lead_id) as leads
-        from na_field_marketing.src_lmh_sfdc l
-        join 
-        na_field_marketing.temp_leads_ite_2018 t
-        on l.lead_id = t.lead_id
-        where lead_stage_to='{}'
-        ;
-    '''.format(lead_stage)
-    df=ds.query_RS(query)
-    print('{} Leads: {}'.format(lead_stage,df['leads'][0]))  
+        ## CREATING YEAR LIST 
+        year_list=[]
+        start_year=2019
+        curr_year=dt.datetime.today().year
+        for year in range(start_year,curr_year+1):
+            year_list.append(year)
+        
+        for persona in personas: 
+            persona_filter_value=persona.replace('_',' ')
+            for year in year_list:
+                try:
+                    query='''
+                            create table na_field_marketing.temp_leads_{}_{} as (
+                            select   distinct lead_id 
+                            from na_field_marketing.src_leads_sfdc l
+                            join 
+                            (
+                            select distinct "lead contact id"
+                            from na_field_marketing.prod_nafm_mt pnm
+                            where "eloqua persona" = '{}' and "email (person)" != '%ibm%'
+                            ) p
+                            on left(l.lead_id,15) = "lead contact id"
+                            where lead_date_created >= '{}-01-01' and lead_date_created <= '{}-12-31'
+                            );
+                            '''.format(persona,year,persona_filter_value,year,year)
+                    ds.query_RS(query,option= 'execute',instance='DS')
+                    print('--------- Leads table created for {} for the year {} ---------'.format(persona,year))  
+                except:
+                    query='drop table na_field_marketing.temp_leads_{}_{};'.format(persona,year)+query
+                    ds.query_RS(query,option= 'execute',instance='DS')    
+                    print('--------- Leads table created for {} for the year {} ---------'.format(persona,year))
+                    
     
+    ### CALCULATIONS 
     
-## CALC FOR CONVERTED LEADS
+    def calculations(self):
+        query='''
+               select distinct "eloqua persona"
+               from na_field_marketing.prod_nafm_mt
+              '''
+        df=ds.query_RS(query)
 
-query='''
-        select count (distinct t.lead_id) as leads
-        from na_field_marketing.temp_leads_ite_2018 t 
-        join na_field_marketing.src_leads_sfdc s on s.lead_id = t.lead_id
-        where s.is_converted is true;
-    '''
-df=ds.query_RS(query)
+        ## CREATING PERSONA LIST OUT OF THE DATAFRAME
+        series=df['eloqua persona']
+        personas=[]
+        for item in series:
+            item=item.replace(' ','_')
+            personas.append(item)
 
-print('Total number of Converted Leads for IT Executive for the year 2018 are {}'.format(df['leads'][0]))
+        ## CREATING YEAR LIST 
+        year_list=[]
+        start_year=2019
+        curr_year=dt.datetime.today().year
+        for year in range(start_year,curr_year+1):
+            year_list.append(year)
+        
+        for persona in personas:     
+            for year in year_list:
+                ### Print the Persona and the Year the data is about
+                
+                print('--------- Lead Report for {} for the year {} ---------'.format(persona,year)) 
+                
+                ### CALCULATION FOR TOTAL NUMBER OF LEADS
+                
+                query='''
+                select count(distinct l.lead_id) as leads
+                from na_field_marketing.src_lmh_sfdc l
+                join 
+                na_field_marketing.temp_leads_{}_{} t
+                on l.lead_id = t.lead_id
+                ;
+                    '''.format(persona,year)
+                df=ds.query_RS(query)
+                print('Total number of leads: {}'.format(df['leads'][0]))
+                
+                ### CALCULATION FOR MAL, SAL, SQL
+
+                lead_stages=['Marketing Accepted','Sales Accepted','Sales Qualified']
+                for lead_stage in lead_stages:
+                    query='''
+                        select  count(distinct l.lead_id) as leads
+                        from na_field_marketing.src_lmh_sfdc l
+                        join 
+                        na_field_marketing.temp_leads_{}_{} t
+                        on l.lead_id = t.lead_id
+                        where lead_stage_to='{}'
+                        ;
+                    '''.format(persona,year,lead_stage)
+                    df=ds.query_RS(query)
+                    print('{} Leads: {}'.format(lead_stage,df['leads'][0])) 
+                    
+                ### CALCULATION FOR CONVERTED LEADS
+                
+                query='''
+                        select count (distinct t.lead_id) as leads
+                        from na_field_marketing.temp_leads_{}_{} t 
+                        join na_field_marketing.src_leads_sfdc s on s.lead_id = t.lead_id
+                        where s.is_converted is true;
+                    '''.format(persona,year)
+                df=ds.query_RS(query)
+
+                print('Total number of Converted Leads: {}'.format(df['leads'][0]))
+            print('\n')
 
 
 
